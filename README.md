3c9db70420b1811179f4cb881e471563016b8435
Konstantin S <havendv@gmail.com>
2023-10-03 01:31:58 +0400

    feat: Released.

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Gtk
-GTK workload and separate `net7.0-gtk` TargetFramework like Tizen.NET to use GTK in single project solutions and differentiate it from `net7.0`
+<b>Gtk</b> is separate TPM for developing Gtk applications within a single project, if there are other TargetFrameworks using the main `net7.0` TPM.
+
+## Usage
+Just install and add `net7.0-gtk` to `TargetFrameworks`.
+
+## Install
+You can install Gtk workload for .NET 6.0/7.0/8.0 by using the installer script.
+- On Linux / macOS:
+```
+curl -sSL https://raw.githubusercontent.com/HavenDV/Gtk/main/scripts/workload-install.sh | sudo bash
+```
+if you want to install a specific version of Gtk workload or install to a specific directory, use the following command:
+```
+curl -sSL https://raw.githubusercontent.com/HavenDV/Gtk/main/scripts/workload-install.sh | bash /dev/stdin -v <version> -d <directory>
+```
+- On Windows:
+```
+Invoke-WebRequest 'https://raw.githubusercontent.com/HavenDV/Gtk/main/scripts/workload-install.ps1' -OutFile 'workload-install.ps1';
+./workload-install.ps1 [-v <version>] [-d <directory>]
+```
+You can see the Gtk workload as follows if it is properly installed.
+```
+PS D:\workspace> dotnet workload list
+
+This command lists only workloads that were installed via `dotnet workload install` in this version of the SDK and not those that were installed via Visual Studio.
+
+Installed Workload Ids
+----------------------
+maui
+gtk
+
+Use `dotnet workload search` to find additional workloads to install.
+
+Updates are avaliable for the following workload(s): maui gtk. Run `dotnet workload update` to get the latest  
+```
+
+## Development
+You can test this project using these commands(tested on macOS) in `src/tasks/tasks` folder:
+```
+dotnet build /t:TestWorkload
+dotnet build /t:WorkloadUninstall
+
+other possible targets(it already included in targets above):
+DownloadDotnetInstall
+DotnetInstall
+WorkloadInstall
+BuildPackages
+CleanArtifactsAndTemporaryFiles
+
+You can override these properties:
+/p:UseCurrentDotnet=true - Will install workload to current dotnet instead downloaded.
+```
+
+### Docs
+Official documentation regarding the design of Workloads and Sdks:
+- https://github.com/dotnet/sdk/tree/main/documentation/general/workloads
+- https://github.com/dotnet/designs/blob/main/accepted/2020/workloads/workloads.md
+- https://github.com/dotnet/designs/blob/main/accepted/2020/workloads/workload-resolvers.md
+- https://github.com/dotnet/designs/blob/main/accepted/2020/workloads/workload-manifest.md
+- https://github.com/dotnet/designs/blob/main/accepted/2021/workloads/workload-installation.md
+- MAUI Workload - https://github.com/dotnet/maui/tree/main/src/Workload
+- Tizen Workload - https://github.com/Samsung/Tizen.NET/tree/main/workload
---
+++ scripts/workload-install.sh
@@ -0,0 +1,222 @@
+#
+# Copyright (c) HavenDV and Samsung Electronics. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
+
+#!/bin/bash -e
+
+MANIFEST_NAME="Gtk.Sdk.Manifest"
+MANIFEST_VERSION="<latest>"
+SOURCE="<auto>"
+DOTNET_INSTALL_DIR="<auto>"
+DOTNET_TARGET_VERSION_BAND="<auto>"
+DOTNET_DEFAULT_PATH_LINUX="/usr/share/dotnet"
+DOTNET_DEFAULT_PATH_MACOS="/usr/local/share/dotnet"
+UPDATE_ALL_WORKLOADS="false"
+
+while [ $# -ne 0 ]; do
+    name=$1
+    case "$name" in
+        -v|--version)
+            shift
+            MANIFEST_VERSION=$1
+            ;;
+        -s|--source)
+            shift
+            SOURCE=$1
+            ;;
+        -d|--dotnet-install-dir)
+            shift
+            DOTNET_INSTALL_DIR=$1
+            ;;
+        -t|--dotnet-target-version-band)
+            shift
+            DOTNET_TARGET_VERSION_BAND=$1
+            ;;
+        -u|--update-all-workloads)
+            shift
+            UPDATE_ALL_WORKLOADS="true"
+            ;;
+        -h|--help)
+            script_name="$(basename "$0")"
+            echo "Gtk Workload Installer"
+            echo "Usage: $script_name [-v|--version <VERSION>] [-d|--dotnet-install-dir <DIR>] [-t|--dotnet-target-version-band <VERSION>]"
+            echo "       $script_name -h|-?|--help"
+            echo ""
+            echo "Options:"
+            echo "  -v,--version <VERSION>                     Use specific VERSION, Defaults to \`$MANIFEST_VERSION\`."
+            echo "  -d,--dotnet-install-dir <DIR>              Dotnet SDK Location installed, Defaults to \`$DOTNET_INSTALL_DIR\`."
+            echo "  -t,--dotnet-target-version-band <VERSION>  Use specific dotnet version band for install location, Defaults to \`$DOTNET_TARGET_VERSION_BAND\`."
+            exit 0
+            ;;
+        *)
+            echo "Unknown argument \`$name\`"
+            exit 1
+            ;;
+    esac
+
+    shift
+done
+
+function read_dotnet_link() {
+    cd -P "$(dirname "$1")"
+    dotnet_file="$PWD/$(basename "$1")"
+    while [[ -h "$dotnet_file" ]]; do
+        cd -P "$(dirname "$dotnet_file")"
+        dotnet_file="$(readlink "$dotnet_file")"
+        cd -P "$(dirname "$dotnet_file")"
+        dotnet_file="$PWD/$(basename "$dotnet_file")"
+    done
+    echo $PWD
+}
+
+function error_permission_denied() {
+    echo "No permission to install manifest. Try again with sudo."
+    exit 1
+}
+
+function ensure_directory() {
+    if [ ! -d $1 ]; then
+        mkdir -p $1 || error_permission_denied
+    fi
+    [ ! -w $1 ] && error_permission_denied
+}
+
+# Check dotnet install directory.
+if [[ "$DOTNET_INSTALL_DIR" == "<auto>" ]]; then
+    if [[ -n "$DOTNET_ROOT" && -d "$DOTNET_ROOT" ]]; then
+        DOTNET_INSTALL_DIR=$DOTNET_ROOT
+    elif [[ -d "$DOTNET_DEFAULT_PATH_LINUX" ]]; then
+        DOTNET_INSTALL_DIR=$DOTNET_DEFAULT_PATH_LINUX
+    elif [[ -d "$DOTNET_DEFAULT_PATH_MACOS" ]]; then
+        DOTNET_INSTALL_DIR=$DOTNET_DEFAULT_PATH_MACOS
+    elif [[ -n "$(which dotnet)" ]]; then
+        DOTNET_INSTALL_DIR=$(read_dotnet_link $(which dotnet))
+    fi
+fi
+if [ ! -d $DOTNET_INSTALL_DIR ]; then
+    echo "No installed dotnet \`$DOTNET_INSTALL_DIR\`."
+    exit 1
+fi
+
+# Check installed dotnet version
+DOTNET_COMMAND="$DOTNET_INSTALL_DIR/dotnet"
+
+if [ ! -x "$DOTNET_COMMAND" ]; then
+    echo "$DOTNET_COMMAND command not found"
+    exit 1
+fi
+
+function install_gtkworkload() {
+    DOTNET_VERSION=$1
+    SOURCE=$2
+    IFS='.' read -r -a array <<< "$DOTNET_VERSION"
+    CURRENT_DOTNET_VERSION=${array[0]}
+    DOTNET_VERSION_BAND="${array[0]}.${array[1]}.${array[2]:0:1}00"
+
+    # Reset local variables
+    if [[ "$UPDATE_ALL_WORKLOADS" == "true" ]]; then
+        DOTNET_TARGET_VERSION_BAND="<auto>"
+        MANIFEST_VERSION="<latest>"
+    fi
+
+    # Check version band
+    if [[ "$DOTNET_TARGET_VERSION_BAND" == "<auto>" ]]; then
+        if [[ "$CURRENT_DOTNET_VERSION" -ge "7" ]]; then
+            if [[ "$DOTNET_VERSION" == *"-preview"* || $DOTNET_VERSION == *"-rc"* || $DOTNET_VERSION == *"-alpha"* ]] && [[ ${#array[@]} -ge 4 ]]; then
+                DOTNET_TARGET_VERSION_BAND="$DOTNET_VERSION_BAND${array[2]:3}.${array[3]}"
+            else
+                DOTNET_TARGET_VERSION_BAND=$DOTNET_VERSION_BAND
+            fi
+        else
+            DOTNET_TARGET_VERSION_BAND=$DOTNET_VERSION_BAND
+        fi
+    fi
+
+    # Check latest version of manifest.
+    if [[ "$MANIFEST_VERSION" == "<latest>" ]]; then
+        MANIFEST_VERSION="0.2.0"
+        if [ ! "$MANIFEST_VERSION" ]; then
+            MANIFEST_VERSION=$(curl -s https://api.nuget.org/v3-flatcontainer/$MANIFEST_NAME/index.json | grep \" | tail -n 1 | tr -d '\r' | xargs)
+            if [[ -n $MANIFEST_VERSION ]]; then
+                echo "Return cached latest version: $MANIFEST_VERSION"
+            else
+                echo "Failed to get the latest version of $MANIFEST_NAME."
+                return
+            fi
+        fi
+    fi
+
+    # Check workload manifest directory.
+    SDK_MANIFESTS_DIR=$DOTNET_INSTALL_DIR/sdk-manifests/$DOTNET_TARGET_VERSION_BAND
+    ensure_directory $SDK_MANIFESTS_DIR
+
+    TMPDIR=$(mktemp -d)
+
+    echo "Installing $MANIFEST_NAME/$MANIFEST_VERSION to $SDK_MANIFESTS_DIR..."
+
+    # Download and extract the manifest nuget package.
+    if [[ "$SOURCE" == "<auto>" ]]; then
+      curl -s -o $TMPDIR/manifest.zip -L https://www.nuget.org/api/v2/package/$MANIFEST_NAME/$MANIFEST_VERSION
+    else
+      cp -f $SOURCE/$MANIFEST_NAME.$MANIFEST_VERSION.nupkg $TMPDIR/manifest.zip
+    fi
+
+    unzip -qq -d $TMPDIR/unzipped $TMPDIR/manifest.zip
+    if [ ! -d $TMPDIR/unzipped/data ]; then
+        echo "No such files to install."
+        return
+    fi
+    chmod 744 $TMPDIR/unzipped/data/*
+
+    # Copy manifest files to dotnet sdk.
+    mkdir -p $SDK_MANIFESTS_DIR/gtk.sdk.manifest
+    cp -f $TMPDIR/unzipped/data/* $SDK_MANIFESTS_DIR/gtk.sdk.manifest/
+
+    if [ ! -f $SDK_MANIFESTS_DIR/gtk.sdk.manifest/WorkloadManifest.json ]; then
+        echo "Installation is failed."
+        return
+    fi
+
+    # Install workload packs.
+    if [ -f global.json ]; then
+        CACHE_GLOBAL_JSON="true"
+        mv global.json global.json.bak
+    else
+        CACHE_GLOBAL_JSON="false"
+    fi
+    dotnet new globaljson --sdk-version $DOTNET_VERSION
+    
+    if [[ "$SOURCE" == "<auto>" ]]; then
+      $DOTNET_INSTALL_DIR/dotnet workload install gtk --skip-manifest-update
+    else
+      $DOTNET_INSTALL_DIR/dotnet workload install gtk --skip-manifest-update --source $SOURCE
+    fi
+    
+    # Clean-up
+    rm -fr $TMPDIR
+    rm global.json
+    if [[ "$CACHE_GLOBAL_JSON" == "true" ]]; then
+        mv global.json.bak global.json
+    fi
+
+    echo "Done installing Gtk workload $MANIFEST_VERSION"
+    echo ""
+}
+
+if [[ "$UPDATE_ALL_WORKLOADS" == "true" ]]; then
+    INSTALLED_DOTNET_SDKS=$($DOTNET_COMMAND --list-sdks | sed -n '/^6\|^7/p' | sed 's/ \[.*//g')
+else
+    INSTALLED_DOTNET_SDKS=$($DOTNET_COMMAND --version)
+fi
+
+if [ -z "$INSTALLED_DOTNET_SDKS" ]; then
+    echo ".NET SDK version 6 or later is required to install Gtk Workload."
+else
+    for DOTNET_SDK in $INSTALLED_DOTNET_SDKS; do
+        echo "Check Gtk Workload for sdk $DOTNET_SDK."
+        install_gtkworkload $DOTNET_SDK $SOURCE
+    done
+fi
+
+echo "DONE"
